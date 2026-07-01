42931c312446081fc396923a4d38a2203c2428c1
Hyria Roman <hyrya.roman@gmail.com>
2026-07-01 13:41:23 +0300

    app: basic layout + styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,9 @@
+import css from "./App.module.css";
+
 export default function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <div className={css.app}>
+      <header className={css.toolbar}></header>
+    </div>
+  );
 }
